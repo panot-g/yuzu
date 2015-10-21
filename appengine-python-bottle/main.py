2a64cff09b207c16c2d0521cf74a12a6d2b64d06
Clement Skau <cskau@google.com>
2015-10-21 18:26:19 +0900

    Adds echoing chat end point.

--- appengine-python-bottle/main.py
+++ appengine-python-bottle/main.py
@@ -1,18 +1,44 @@
-from bottle import Bottle, view
+from bottle import Bottle, view, request
 
+from collections import deque
+import json
+
+
+# App Engine ?
 app = Bottle()
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+
+# This is the global, shared list of the most recent messages.
+MESSAGES = deque(maxlen=20)
 
 
 @app.route('/')
 @view('views/index')
-def index(name='World'):
-    return dict(name=name)
+def index():
+    #host = bottle.request.get_header('host')
+    host = ''
+    return dict(host=host)
+
+@app.route('/chat')
+def chat_get():
+    return {'messages': list(MESSAGES)}
+
+@app.post('/chat', method='POST')
+def chat_post():
+    try:
+        jsonMessage = request.json
+    except ValueError as e:
+        return {'status': 'ERROR', 'description': 'Could not parse JSON.'}
+
+    if not jsonMessage:
+        return {'status': 'ERROR', 'description': 'No JSON message received.'}
+
+    MESSAGES.append({'message': jsonMessage['message']})
+
+    return {'status': 'OK'}
 
 # Define an handler for 404 errors.
 @app.error(404)
 def error_404(error):
     """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.'
+    return 'Sorry, nothing at this URL. (404)'
